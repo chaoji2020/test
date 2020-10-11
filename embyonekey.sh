d904d0a62e397d83f6a9e3f89413422599de3052
chaoji2020 <72678441+chaoji2020@users.noreply.github.com>
2020-10-11 11:41:27 +0800

    Update embyonekey.sh

--- embyonekey.sh
+++ embyonekey.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 cd /var/packages/EmbyServer/target/mono/bin
-curl https://raw.githubusercontent.com/chaoji2020/embyonekey/main/mb3admin.com.cert.pem >> /etc/ssl/certs/ca-certificates.crt
+curl https://raw.githubusercontent.com/chaoji2020/test/main/mb3admin.com.cert.pem >> /etc/ssl/certs/ca-certificates.crt
 ./cert-sync /etc/ssl/certs/ca-certificates.crt
 cd /usr/local/etc/nginx/conf.d/*-*-*-*
-wget https://raw.githubusercontent.com/chaoji2020/embyonekey/main/user.conf
+wget https://raw.githubusercontent.com/chaoji2020/test/main/user.conf
 nginx -s reload
 
